2331556d33d468e793c452534b058d88ae7b28db
rania <raniaarbash@hotmail.com>
2021-11-04 19:09:31 -0400

    Update with interface in Fragment class

--- app/src/main/java/com/example/fragmentexample1/FragmentTwo.java
+++ app/src/main/java/com/example/fragmentexample1/FragmentTwo.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,16 +12,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentTwo extends Fragment {
+public class FragmentTwo extends Fragment implements View.OnClickListener {
+
+Button button;
+int id;
+
+    FragmentTwo(int id){
+        this.id = id;
+    }
+    public interface FragmentListener{
+        public void onReturn(String value);
+    }
+
+    public FragmentListener listener;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.fragment_two,container,false);
-        int id = getArguments().getInt("imageid");
+       // int id = getArguments().getInt("imageid");
         ImageView image = (ImageView) v.findViewById(R.id.imageInFragment2);
         image.setImageResource(id);
+        button = (Button) v.findViewById(R.id.button);
+        button.setOnClickListener(this);
         return v;
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        listener.onReturn("Value from Fragment");
 
     }
 }
--- app/src/main/java/com/example/fragmentexample1/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample1/MainActivity.java
@@ -7,20 +7,34 @@ import androidx.fragment.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentTwo.FragmentListener {
     FragmentManager fm;
+    FragmentTwo fragmentTwo;
+    EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
         fm = getSupportFragmentManager();
 
     }
 
     public void AddFragmentTwo(View view) {
-        Bundle bundle1 = new Bundle();
-        bundle1.putInt("imageid",R.drawable.img);
-        fm.beginTransaction().add(R.id.fragment_container_view,FragmentTwo.class,bundle1).commit();
+//        Bundle bundle1 = new Bundle();
+//        bundle1.putInt("imageid",R.drawable.img);
+         fragmentTwo = new FragmentTwo(R.drawable.img);
+        fragmentTwo.listener = this;
+
+        fm.beginTransaction().add(R.id.fragment_container_view,fragmentTwo,"tag").commit();
+    }
+
+    @Override
+    public void onReturn(String value) {
+        text.setText(value);
+        getSupportFragmentManager().beginTransaction().remove(fragmentTwo).commit();
+
     }
 }
